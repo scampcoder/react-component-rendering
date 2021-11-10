aff54bbe023ecd4b5c3b6739720d8e3939e35097
Victoria Kee <victoriakee95@gmail.com>
2021-11-10 16:20:49 -0600

    Create planet component

--- start/app.js
+++ start/app.js
@@ -70,10 +70,20 @@ const planets = [
 // =============================================================
 
 // 1: Create a 'Planet' component that renders a planet card
+const Planet = (props) => {
+  return (
+    <div className="card">
+        <div>
+          <img src={props.url} alt={props.name}/>
+        </div>
+        <h2>{props.name}</h2>
+        <p>{props.desc}</p>
+        <h3>Planet Profile</h3>
+        <ul>
+          <li><strong>Diameter:</strong> {props.diameter}</li>
+          <li><strong>Moons:</strong> {props.moons}</li>
+        </ul>
+    </div>
+  );
+}
 
-
-// 2: Create a container component that iterates over the planets array 
-//    and renders a 'Planet' component for each object in the array 
-
-
-// 3: Render the container component to the DOM
